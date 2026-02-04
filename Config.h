8b6cf277ed31adb429d860b0121c8a061872ecf5
r.roux <roman.roux@student.isae-supaero.fr>
2026-02-04 16:28:52 +0100

    Starting to add colonies, interactions between ants, food, and basic neural network and most fitting algorithms

--- Config.h
+++ Config.h
@@ -3,13 +3,13 @@
 
 namespace Config {
 
-constexpr int WINDOW_WIDTH = 1280;
-constexpr int WINDOW_HEIGHT = 720;
+constexpr int WINDOW_WIDTH = 800;
+constexpr int WINDOW_HEIGHT = 800;
 
 constexpr int MAX_ANTS = 500000;
 
 struct Settings {
-  int antCount = 2000000;
+  int antCount = 1000000;
   float antSize = 0.2f;
   float antSpeed = 1.0f;
   float sensorAngle = 0.78f;
